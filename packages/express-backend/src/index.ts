16f81d4286c1ef087943dbfff410abf618c15625
Karl Thaler <karlthaler5@gmail.com>
2024-02-24 16:48:08 -0800

    and login and register endpoints

--- packages/express-backend/src/index.ts
+++ packages/express-backend/src/index.ts
@@ -12,50 +12,84 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-connect("FoodBracket")
+connect("FoodBracket");
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
 
 app.get("/", (req: Request, res: Response) => {
-    res.send('<h1>Hello World!</h1>');
+  res.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/profiles/:userid", (req: Request, res: Response) => {
-    const { userid } = req.params;
-    profiles
-        .get(userid)
-        .then((profile: Profile) => res.json(profile))
-        .catch((err) => res.status(404).end());
+  const { userid } = req.params;
+  profiles
+    .get(userid)
+    .then((profile: Profile) => res.json(profile))
+    .catch((err) => res.status(404).end());
 });
 
 app.post("/api/profiles", (req: Request, res: Response) => {
-    const newProfile = req.body;
-    profiles
-        .create(newProfile)
-        .then((profile: Profile) => res.status(201).send(profile))
-        .catch((err) => res.status(500).end());
-})
+  const newProfile = req.body;
+  profiles
+    .create(newProfile)
+    .then((profile: Profile) => res.status(201).send(profile))
+    .catch((err) => res.status(500).end());
+});
 
 app.put("/api/profiles/:userid", (req: Request, res: Response) => {
-    const { userid } = req.params;
-    const newProfile = req.body;
+  const { userid } = req.params;
+  const newProfile = req.body;
 
-    profiles
-        .update(userid, newProfile)
-        .then((profile: Profile) => res.json(profile))
+  profiles
+    .update(userid, newProfile)
+    .then((profile: Profile) => res.json(profile))
+    .catch((err) => res.status(404).end());
+});
+
+app.get("/api/users", (req: Request, res: Response) => {
+  credentials
+    .index()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(404).end());
+});
+
+app.delete("/api/users", (req: Request, res: Response) => {
+    credentials
+        .deleteCredentials()
+        .then(() => res.status(204).end())
         .catch((err) => res.status(404).end());
 });
 
-app.post("/api/register", (req: Request, res: Response) => {
-    const { username, password }: { username: string, password: string } = req.body;
-    console.log("heeeere", username, password);
-    credentials
-        .create(username, password)
-        .then((creds) => generateAccessToken(creds.username))
-        .then((token: any) => { 
-            res.status(201).send({ token: token });
-    })
-})
 
+
+app.post("/api/register", (req: Request, res: Response) => {
+  const { username, password }: { username: string; password: string } =
+    req.body;
+  if (!username || !password) {
+    res.status(400).send("must provide username and password");
+    return;
+  } else {
+    credentials
+      .create(username, password)
+      .then((creds) => generateAccessToken(creds.username))
+      .then((token) => res.status(201).send({ token: token }))
+      .catch((err) => res.status(401).send("username exists"));
+  }
+});
+
+
+app.post("/api/login", (req: Request, res: Response) => {
+  const { username, password }: { username: string; password: string } =
+    req.body;
+  if (!username || !password) {
+    res.status(400).send("must provide username and password");
+    return;
+  }
+  credentials
+    .verify(username, password)
+    .then((creds: Credential) => generateAccessToken(creds.username))
+    .then((token) => res.status(200).send({ token: token }))
+    .catch((err) => res.status(401).send("Unauthorized"));
+});
--- packages/express-backend/src/services/credentials.ts
+++ packages/express-backend/src/services/credentials.ts
@@ -3,30 +3,58 @@ import CredentialModel from "../models/mongo/credential";
 import bcrypt from "bcrypt";
 
 export function create(username: string, password: string) {
-    return new Promise<Credential>((resolve, reject) => {
-      if (!username || !password) {
-        reject("must provide username and password");
-      }
-      CredentialModel
-        .find({ username })
-        .then((found: Credential[]) => {
-          if (found.length) reject("username exists");
-        })
-        .then(() =>
-          bcrypt
-            .genSalt(10)
-            .then((salt: string) => bcrypt.hash(password, salt))
-            .then((hashedPassword: string) => {
-              const creds = new CredentialModel({
-                username,
-                hashedPassword
-              });
-              creds.save().then((created: Credential) => {
-                if (created) resolve(created);
-              });
-            })
-        );
-    });
-  }
+  return new Promise<Credential>((resolve, reject) => {
+    if (!username || !password) {
+      reject("must provide username and password");
+    }
+    CredentialModel
+      .find({ username })
+      .then((found: Credential[]) => {
+        if (found.length) reject("username exists");
+      })
+      .then(() =>
+        bcrypt
+          .genSalt(10)
+          .then((salt: string) => bcrypt.hash(password, salt))
+          .then((hashedPassword: string) => {
+            const creds = new CredentialModel({
+              username,
+              hashedPassword
+            });
+            creds.save().then((created: Credential) => {
+              if (created) resolve(created);
+            });
+          })
+      );
+  });
+}
 
-export default { create };
+export function verify(username: string, password: string): Promise<String> {
+  return new Promise<String>((resolve, reject) => {
+    CredentialModel
+      .find({ username })
+      .then((found) => {
+        if (found && found.length == 1) return found[0];
+        else return reject("Invalid username or password");
+      })
+      .then((credsOnFile) => {
+        if (credsOnFile)
+          return bcrypt.compare(password, credsOnFile.hashedPassword);
+        else reject("Invalid username or password");
+      })
+      .then((match) => {
+        if (match) resolve(username);
+        else reject("Invalid username or password");
+      });
+  });
+}
+
+function index(): Promise<Credential[]> {
+  return CredentialModel.find();
+}
+
+function deleteCredentials(): Promise<any> {
+  return CredentialModel.deleteMany();
+}
+
+export default { create, verify, index, deleteCredentials };

9aeaefcfbd796d69f4032491108c960fac6c0f3d
Karl Thaler <karlthaler5@gmail.com>
2024-03-09 11:33:31 -0800

    Add restaurant panels that can be selected, and triggers the next round

--- packages/lit-frontend/src/views/bracket-page.ts
+++ packages/lit-frontend/src/views/bracket-page.ts
@@ -1,17 +1,23 @@
 import { css, html } from "lit";
 import * as App from "../app";
-import { property } from "lit/decorators.js";
+import { property, state } from "lit/decorators.js";
 import { Restaurant } from "ts-models";
 import "../components/restaurant-panel.ts";
 
 export class BracketPageElement extends App.View {
   @property({ type: Array })
   cards_selected: Restaurant[] = [];
+  
+  @state()
+  _round: number = 0;
 
   @property()
   get restaurants() {
     return this.getFromModel("selected") as Restaurant[];
   }
+
+  @property({ type: Boolean})
+  bracket_started = false;
 
   static styles = css`
     .bracket {
@@ -71,13 +77,47 @@ export class BracketPageElement extends App.View {
       <button class="tournament-button" @click=${this.beginTournament}>
         Begin Tournament
       </button>
-      <restaurant-panel side="left"></restaurant-panel>
-      <restaurant-panel side="right"></restaurant-panel>
+      ${this.bracket_started ? html`
+      <restaurant-panel side="left" .restaurant=${this.cards_selected[this._round * 2]}></restaurant-panel>
+      <restaurant-panel side="right" .restaurant=${this.cards_selected[(this._round * 2) + 1]}></restaurant-panel>`
+      : html``}
     `;
   }
 
+  connectedCallback(): void {
+    super.connectedCallback();
+    document.addEventListener("panel-clicked", this.handlePanelClicked.bind(this) as unknown as EventListener);
+  }
+
+  disconnectedCallbackconnectedCallback(): void {
+    document.removeEventListener("panel-clicked", this.handlePanelClicked.bind(this) as unknown as EventListener);
+    super.disconnectedCallback();
+  }
+
+  async handlePanelClicked(e: CustomEvent) {
+    const restaurant = e.detail as Restaurant;
+    if (!restaurant) return;
+
+    const leftPanel = this.shadowRoot?.querySelector("restaurant-panel[side=left]") as any;
+    const rightPanel = this.shadowRoot?.querySelector("restaurant-panel[side=right]") as any;
+
+    if(leftPanel && rightPanel) {
+      await Promise.all([leftPanel.closePanel(), rightPanel.closePanel()]);
+    }
+
+    this._round++;
+    this.requestUpdate();
+
+    this.updateComplete.then(() => {
+      if(leftPanel && rightPanel) {
+        leftPanel.openPanel();
+        rightPanel.openPanel();
+      }
+    });
+  }
+
   beginTournament() {
-    console.log("Tournament started");
+    this.bracket_started = true;
   }
 }
 customElements.define("bracket-page", BracketPageElement);
--- packages/lit-frontend/src/views/food-bracket.ts
+++ packages/lit-frontend/src/views/food-bracket.ts
@@ -1,23 +1,49 @@
-import { html } from 'lit';
-import '../components/header.ts';
-import './main-page.ts';
-import '../components/vaadin-router.ts';
+import { css, html } from "lit";
+import "../components/header.ts";
+import "./main-page.ts";
+import "../components/vaadin-router.ts";
 // MVU
 import * as App from "../app";
 import routes from "../routes";
 import update from "../update";
 
 export class FoodBracketElement extends App.Main {
-    constructor() {
-        super(update);
-    }
-    
-    render() {
-        return html`
-            <header-element></header-element>
-            <vaadin-router .routes=${routes}> </vaadin-router>
-        `;
-    }
+  constructor() {
+    super(update);
+  }
+
+  render() {
+    return html`
+      <div class="container">
+        <header-element></header-element>
+        <div class="main">
+        <vaadin-router .routes=${routes}></vaadin-router>
+  </div>
+      </div>
+    `;
+  }
+
+static styles = css`
+  :host {
+    --header-height: 70px; /* Adjust based on your header's size */
+  }
+
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  .container {
+    display: grid;
+    grid-template-rows: var(--header-height) 1fr;
+    height: 100%; /* Ensure the container takes full height of its parent */
+  }
+
+  .main {
+    min-height: 0; /* This ensures that 1fr works correctly in all browsers */
+  }
+`;
 }
 
-customElements.define('food-bracket', FoodBracketElement);
+customElements.define("food-bracket", FoodBracketElement);
